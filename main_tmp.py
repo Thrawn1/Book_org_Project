6c10c5e9c6f3a0d6decc9c21f75780d495197a35
Thrawn1 <basil.eremin@mail.ru>
2016-04-01 16:06:34 +0300

    Реализован простейший поиск по фамилии автора.

--- main_tmp.py
+++ main_tmp.py
@@ -1,23 +1,26 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import shelve
-from s_vol_c import *
-from m_vol_c import *
 from new_record import *
-j = new_record()
-K=[]
-db_f = shelve.open('book_db_2')
-for key in db_f:
-        K.append(key)
-        print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')    
-print(K)
+from search import *
+print('Выберете действие:\n1.Внесесение записи в базу данных\n2.Поиск книг в базе данных(не работает)\n3.Изменение в записи базы данных(не работает)\n4.Удаление записи из базы данных(не работает)')
+in00 = input('Введите нужны пункт меню:')
+if in00 == '1':
+        j = new_record()
+        K=[]
+        db_f = shelve.open('book_db_2')
+        print('Последние 15 записей:')
+        for key in db_f:
+                K.append(key)
+                li = 0
+                if li <15:
+                        print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')
+                else: pass
+        print('Всего записей в базе данных:',len(K))
 
-#db = [book0,book1,book2,book3,book4,book5]
-#R = []
-#j = input('Введите имя автора:')
-#for b in db:
-#	c = b.author_family()
-#	if j == c:
-#		R.append(b.author_initial()+' ; '+ b.name)
-#	else: pass
-#print(R) 
+elif in00 =='2':
+        search_full_name()
+elif in00 =='3':
+        print('ВРЕМЕННО НЕ РАБОТАЕТ')
+elif in00 =='4':
+        print('ВРЕМЕННО НЕ РАБОТАЕТ')
---
+++ search.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+from s_vol_c import *
+from m_vol_c import *
+import shelve
+def search_full_name():
+    R = []
+    K = []
+    db_f = shelve.open('book_db_2')
+    for key in db_f:
+                K.append(key)
+    j = input('Введите имя автора:')
+    for b in K:
+            c = db_f[b].author_family()
+            if j == c:
+                R.append(db_f[b].author_initial()+' ; '+ db_f[b].name)
+            else: pass
+    print(R)
+    
