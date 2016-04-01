cbe2d5cce014cff7f239d9b79647c457330c3249
Thrawn1 <basil.eremin@mail.ru>
2016-04-01 19:15:19 +0300

    Добавление поиска по названию книги. Создание меню поиска

--- search.py
+++ search.py
@@ -9,7 +9,7 @@ def search_family():
     db_f = shelve.open('book_db_2')
     for key in db_f:
                 K.append(key)
-    j = input('Введите имя автора:')
+    j = input('\nВведите имя автора:')
     for b in K:
             c = db_f[b].author_family()
             if j == c:
@@ -20,4 +20,22 @@ def search_family():
         print(R)
     else:
         print('Ничего не найдено!')
-    
+
+def search_name():
+    R = []
+    K = []
+    db_f = shelve.open('book_db_2')
+    for key in db_f:
+                K.append(key)
+    j = input('\nВведите имя название книги:')
+    for b in K:
+            c = db_f[b].name
+            print(c)
+            if j == c:
+                R.append(db_f[b].author_initial()+' ; '+ db_f[b].name)
+            else:
+                pass
+    if len(R) !=0:
+        print(R)
+    else:
+        print('Ничего не найдено!')
