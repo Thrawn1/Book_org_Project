c322c515afac7f1789c7605107ab5cdd5b905073
Thrawn1 <basil.eremin@mail.ru>
2016-04-04 00:39:41 +0300

    ...

--- main_tmp.py
+++ main_tmp.py
@@ -5,8 +5,8 @@ from new_record import *
 from search import *
 from protect_fields import *
 
-print('Выберете действие:\n1.Внесесение записи в базу данных\n2.Поиск книг в базе данных(не работает)\n3.Изменение в записи базы данных(не работает)\n4.Удаление записи из базы данных(не работает)')
-in00 = input('Введите нужны пункт меню:')
+print('Выберете действие:\n\n\n1.Внесесение записи в базу данных\n\n2.Поиск книг в базе данных\n\n3.Изменение в записи базы данных(не работает)\n\n4.Удаление записи из базы данных(не работает)')
+in00 = input('\nВведите нужны пункт меню:')
 if in00 == '1':
         j = new_record()
         K=[]
@@ -32,10 +32,28 @@ elif in00 =='2':
                         for key in z:
                                 print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')
                 if in_1 == '2':
-                        z = search_family(int(in_2))
-                        db_f = shelve.open('book_db_2')
-                        for key in z:
-                                print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')
+                        print('\n\nВыберете вариант поиска по фамилии:\n\n1.Поиск только по фамилии2.Поиск по фамилии и имени\n\n3.Поиск по Фамилии и инициалам\n\n4.Поиск по имени-отчеству\n\n5. *Произвольный поиск(будет реализован много позднее\n\n')# - в нем поиск будет производится в рамках введеных символов, без обязательной привязки к полям')
+                        in_3 = input('Введите номер пункта меню:')
+                        if in_3 == '1':
+                                z = search_family(int(in_2))
+                                db_f = shelve.open('book_db_2')
+                                for key in z:
+                                        print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')
+                        elif in_3 == '2':
+                                z = search_full_author(int(in_2))
+                                db_f = shelve.open('book_db_2')
+                                for key in z:
+                                        print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')
+                        elif in_3 == '3':
+                                z = search_initials_family(int(in_2))
+                                db_f = shelve.open('book_db_2')
+                                for key in z:
+                                        print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n')
+                        elif in_3 == '4':
+                                z = search_middle name(int(in_2))
+                                db_f = shelve.open('book_db_2')
+                                for key in z:
+                                        print('\t',db_f[key].author,db_f[key].name,db_f[key].year_p,('Полка №'+str(db_f[key].location)),'\n') 
                 else: print('Ошибка')
         else:
                 pass
